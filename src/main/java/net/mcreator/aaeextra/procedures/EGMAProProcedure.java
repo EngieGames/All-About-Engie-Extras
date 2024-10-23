f9936dd591421c9acd9d0dadc9a077c5e50c7660
DevEngie <engifr.games@gmail.com>
2024-10-23 17:55:17 -0500

    Main

---
+++ src/main/java/net/mcreator/aaeextra/procedures/EGMAProProcedure.java
@@ -0,0 +1,59 @@
+package net.mcreator.aaeextra.procedures;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+public class EGMAProProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity.getDisplayName().getString()).equals("clippedbyengie")) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode adventure");
+				}
+			}
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("egmc, egms, egmsp for other modes"), true);
+		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] clippedbyengie")) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode adventure");
+				}
+			}
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("egmc, egms, egmsp for other modes"), true);
+		} else if ((entity.getDisplayName().getString()).equals("Dev")) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode adventure");
+				}
+			}
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("egmc, egms, egmsp for other modes"), true);
+		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] Dev")) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode adventure");
+				}
+			}
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("egmc, egms, egmsp for other modes"), true);
+		} else {
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal("Sorry! This command is limited to just Engie."), true);
+		}
+	}
+}
