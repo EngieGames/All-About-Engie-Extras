c97af8b15423c65e72e372e9063dfc972bd98ae5
DevEngie <engifr.games@gmail.com>
2024-10-23 18:09:58 -0500

    1.19.2

--- src/main/java/net/mcreator/aaeextra/procedures/EGMCProProcedure.java
+++ src/main/java/net/mcreator/aaeextra/procedures/EGMCProProcedure.java
@@ -2,7 +2,10 @@ package net.mcreator.aaeextra.procedures;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 public class EGMCProProcedure {
 	public static void execute(Entity entity) {
@@ -11,38 +14,46 @@ public class EGMCProProcedure {
 		if ((entity.getDisplayName().getString()).equals("clippedbyengie")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null)
-					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity clippedbyengie run gamemode creative");
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode creative");
+				}
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(new TextComponent("egms, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(Component.literal("egms, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] clippedbyengie")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null)
-					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity clippedbyengie run gamemode creative");
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode creative");
+				}
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(new TextComponent("egms, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(Component.literal("egms, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("Dev")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null)
-					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity Dev run gamemode creative");
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode creative");
+				}
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(new TextComponent("egms, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(Component.literal("egms, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] Dev")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null)
-					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity Dev run gamemode creative");
+				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode creative");
+				}
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(new TextComponent("egms, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(Component.literal("egms, egmsp, egma for other modes"), true);
 		} else {
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(new TextComponent("Sorry! This command is limited to just Engie."), true);
+				_player.displayClientMessage(Component.literal("Sorry! This command is limited to just Engie."), true);
 		}
 	}
 }
--- src/main/java/net/mcreator/aaeextra/procedures/GMSPProProcedure.java
+++ src/main/java/net/mcreator/aaeextra/procedures/GMSPProProcedure.java
@@ -1,6 +1,9 @@
 package net.mcreator.aaeextra.procedures;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 public class GMSPProProcedure {
 	public static void execute(Entity entity) {
@@ -8,8 +11,10 @@ public class GMSPProProcedure {
 			return;
 		{
 			Entity _ent = entity;
-			if (!_ent.level.isClientSide() && _ent.getServer() != null)
-				_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "gamemode spectator @p");
+			if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "gamemode spectator @p");
+			}
 		}
 	}
 }
