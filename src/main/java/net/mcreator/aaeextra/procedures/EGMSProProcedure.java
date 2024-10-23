b35e7c2e0aa318a4aeb8bca6f3adbc968b204d36
DevEngie <engifr.games@gmail.com>
2024-10-23 18:02:29 -0500

    1.18.2

--- src/main/java/net/mcreator/aaeextra/procedures/EGMSProProcedure.java
+++ src/main/java/net/mcreator/aaeextra/procedures/EGMSProProcedure.java
@@ -2,10 +2,7 @@ package net.mcreator.aaeextra.procedures;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.Component;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.CommandSource;
+import net.minecraft.network.chat.TextComponent;
 
 public class EGMSProProcedure {
 	public static void execute(Entity entity) {
@@ -14,46 +11,38 @@ public class EGMSProProcedure {
 		if ((entity.getDisplayName().getString()).equals("clippedbyengie")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode survival");
-				}
+				if (!_ent.level.isClientSide() && _ent.getServer() != null)
+					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity clippedbyengie run gamemode survival");
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(Component.literal("egmc, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(new TextComponent("egmc, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] clippedbyengie")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity clippedbyengie run gamemode survival");
-				}
+				if (!_ent.level.isClientSide() && _ent.getServer() != null)
+					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity clippedbyengie run gamemode survival");
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(Component.literal("egmc, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(new TextComponent("egmc, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("Dev")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode survival");
-				}
+				if (!_ent.level.isClientSide() && _ent.getServer() != null)
+					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity Dev run gamemode survival");
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(Component.literal("egmc, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(new TextComponent("egmc, egmsp, egma for other modes"), true);
 		} else if ((entity.getDisplayName().getString()).equals("[AAE Developer] Dev")) {
 			{
 				Entity _ent = entity;
-				if (!_ent.level.isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "execute if entity Dev run gamemode survival");
-				}
+				if (!_ent.level.isClientSide() && _ent.getServer() != null)
+					_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "execute if entity Dev run gamemode survival");
 			}
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(Component.literal("egmc, egmsp, egma for other modes"), true);
+				_player.displayClientMessage(new TextComponent("egmc, egmsp, egma for other modes"), true);
 		} else {
 			if (entity instanceof Player _player && !_player.level.isClientSide())
-				_player.displayClientMessage(Component.literal("Sorry! This command is limited to just Engie."), true);
+				_player.displayClientMessage(new TextComponent("Sorry! This command is limited to just Engie."), true);
 		}
 	}
 }
